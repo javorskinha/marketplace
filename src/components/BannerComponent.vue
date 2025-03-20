a5290a58779b18533516c9ae655bedef4480acdb
Javorskinha <andrielijavorski@gmail.com>
2025-03-20 16:47:00 -0300

    create BanneComponent content

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="banner">
+        <div class="banner-content">
+            <div>
+                <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam laudantium vel.</p>
+            </div>
+            <img src="../../public/images/image-example.jpeg" alt="">
+            <div class="buttons">
+                <ButtonComponent text="Shop Now >" class="yellow button"/>
+                <ButtonComponent text="See Category" class="white button"/>
+            </div>
+        </div>
         <div>
             <div v-for="categorie in allCategories" :key="categorie.id">
                 <p>{{ categorie.name }}</p>
@@ -9,6 +20,7 @@
 </template>
 
 <script setup>
+import ButtonComponent from "./ButtonComponent.vue";
 import { getCategories } from "@/services/HttpService.js";
 import { onMounted, ref } from "vue";
 
@@ -23,3 +35,31 @@ async function getAllCategories() {
 
 onMounted(getAllCategories)
 </script>
+
+<style scoped>
+    .banner{
+        background-color: var(--color-purple);
+    }
+
+    .banner-content{
+        padding: 3em 1em;
+        color: var(--color-white);
+    }
+
+    .banner-content img{
+        display: block;
+        place-self: center;
+        border-radius: 50%;
+        margin: 15px 0 15px 0;
+    }
+
+    .buttons{
+        display: block;
+        place-self: center;
+        width: 90%;
+    }
+
+    .button{
+        margin: 3px;
+    }
+</style>
