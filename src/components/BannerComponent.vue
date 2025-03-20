f5b348e35c3cd7fa08ec3f018f979a6ce7e1bc9f
Javorskinha <andrielijavorski@gmail.com>
2025-03-20 16:52:04 -0300

    created CategoriesComponent.vue

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -11,29 +11,11 @@
                 <ButtonComponent text="See Category" class="white button"/>
             </div>
         </div>
-        <div>
-            <div v-for="categorie in allCategories" :key="categorie.id">
-                <p>{{ categorie.name }}</p>
-            </div>
-        </div>
     </div>
 </template>
 
 <script setup>
 import ButtonComponent from "./ButtonComponent.vue";
-import { getCategories } from "@/services/HttpService.js";
-import { onMounted, ref } from "vue";
-
-const allCategories = ref([]);
-
-async function getAllCategories() {
-    const result = await getCategories()
-    console.log(result)
-
-    allCategories.value = result;
-}
-
-onMounted(getAllCategories)
 </script>
 
 <style scoped>
---
+++ src/components/CategoriesComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Shop By Categories</h2>
+        <div v-for="categorie in allCategories" :key="categorie.id">
+            <p>{{ categorie.name }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { getCategories } from "@/services/HttpService.js";
+import { onMounted, ref } from "vue";
+
+const allCategories = ref([]);
+
+async function getAllCategories() {
+    const result = await getCategories()
+    console.log(result)
+
+    allCategories.value = result;
+}
+
+onMounted(getAllCategories)
+</script>
