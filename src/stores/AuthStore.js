e43356a9a4347760c2889e1315e7910450331ca3
Javorskinha <andrielijavorski@gmail.com>
2025-04-02 15:48:59 -0300

    create cart in register

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { postLogin, postRegister } from "@/services/HttpService";
+import { postCart, postLogin, postRegister } from "@/services/HttpService";
 import { computed, ref } from "vue";
 
 export const useAuthStore = defineStore('auth', ()=>{
@@ -26,6 +26,7 @@ export const useAuthStore = defineStore('auth', ()=>{
             const response = await postRegister(userData);
             user.value = response.data;
             await login({ email: userData.email, password: userData.password });
+            await postCart();
         } catch (error){
             console.error('Não foi possivel realizar o cadastro, tente novamente.', error);
             throw error;
