460c35ecd4d767fa2a71d5be1c32d48de89661ae
javorskinha <andrielijavorski@gmail.com>
2025-04-06 16:31:48 -0300

    upgrade header styles

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,10 +2,10 @@
     <nav class="navbar navbar-expand-md bg-light px-3">
         <div class="container-fluid">
             <div class="d-flex align-items-center gap-3">
-                <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
-                    <span class="pi pi-bars "></span>
+                <button class="navbar-toggler d-md-none h-100" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                    <span class="pi pi-bars"></span>
                 </button>
-                <a class="navbar-brand" href="/">S+mart</a>
+                <h1><a class="navbar-brand fw-bold" href="/">S+mart</a></h1>
             </div>
             
             <div class="d-none d-md-flex align-items-center gap-4">
@@ -32,21 +32,26 @@
             </div>
         </div>
     </nav>
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title">S+mart</h5>
+    <div class="offcanvas vh-100 offcanvas-start w-75 transition" tabindex="-1" id="offcanvasNavbar">
+      <div class="offcanvas-header pb-0">
+        <h3 class="offcanvas-title fw-bold">S+mart</h3>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
       </div>
-      <div class="offcanvas-body">
-        <InputComponent type="text" placeholder="Buscar" />
-        <ul class="navbar-nav">
-          <li class="nav-item"><a class="nav-link" href="#">Categorias</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Ofertas</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Conta</a></li>
-        </ul>
-        <a href="/account" class="d-flex gap-2 mt-3">
-          <ButtonComponent text="Entrar" />
-          <ButtonComponent text="Criar Conta" />
+      <div class="offcanvas-body d-flex flex-column justify-content-between">
+        <div>
+            <div  class="position-relative">
+                <InputComponent type="text" placeholder="Buscar"/>
+                <i class="pi pi-search position-absolute end-0 top-50 translate-middle"></i>
+            </div>
+            <ul class="navbar-nav d-flex flex-column justify-content-around">
+              <li class="d-flex align-items-center justify-content-between border-bottom mb-2"><a class="nav-link pb-0" href="#">Categorias</a><i class="pi pi-chevron-right"></i></li>
+              <li class="d-flex align-items-center justify-content-between border-bottom mb-2"><a class="nav-link pb-0" href="#">Ofertas</a><i class="pi pi-chevron-right"></i></li>
+              <li class="d-flex align-items-center justify-content-between border-bottom mb-2"><a class="nav-link pb-0" href="#">Conta</a><i class="pi pi-chevron-right"></i></li>
+            </ul>
+        </div>
+        <a href="/account" class="d-flex flex-column gap-2 mt-3 nav-link">
+          <ButtonComponent text="Entrar" class="btn btn-outline-primary"/>
+          <ButtonComponent text="Criar Conta" class="btn btn-outline-secondary bg-info"/>
         </a>
       </div>
     </div>
@@ -56,3 +61,9 @@
 import ButtonComponent from './ButtonComponent.vue';
 import InputComponent from './InputComponent.vue';
 </script>
+
+<style>
+    .transition{
+        transition: transform 0.7s ease-in-out;
+    }
+</style>
