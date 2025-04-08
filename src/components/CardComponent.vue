4d0380763917b6759452d66f2f6727d05d817743
Javorskinha <andrielijavorski@gmail.com>
2025-04-08 16:25:50 -0300

    height config

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -4,18 +4,22 @@
             <img :src="src" class="card-img-top" :alt="alt">
             <span class="badge bg-danger position-absolute top-0 start-0 m-2">novo{{ marker }}</span>
         </div>
-        <div class="card-body">
-            <h5 class="card-title">{{ name }}</h5>
-            <p class="card-text text-muted small card-text-limit">{{ description }}</p>
-            <p class="card-text fw-bold fs-5 text-primary">R$ {{ price }}</p>
-            <div class="d-flex flex-column gap-2">
-                <ButtonComponent
-                text="Adicionar ao Carrinho"
-                class="btn btn-info"
-                @click="$emit('addtocart')"
-                icon="pi pi-shopping-cart"
-                />
-                <ButtonComponent text="Lista de Desejos" class="btn btn-outline-secondary" icon="pi pi-heart"/>
+        <div class="card-body d-flex flex-column justify-content-between">
+            <div>
+                <h5 class="card-title">{{ name }}</h5>
+                <p class="card-text text-muted small card-text-limit">{{ description }}</p>
+            </div>
+            <div>
+                <p class="card-text fw-bold fs-5 text-primary">R$ {{ price }}</p>
+                <div class="d-flex flex-column gap-2">
+                    <ButtonComponent
+                    text="Adicionar ao Carrinho"
+                    class="btn btn-info"
+                    @click="$emit('addtocart')"
+                    icon="pi pi-shopping-cart"
+                    />
+                    <ButtonComponent text="Lista de Desejos" class="btn btn-outline-secondary" icon="pi pi-heart"/>
+                </div>
             </div>
         </div>
     </div>
