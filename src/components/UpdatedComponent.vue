7b397742c1708e77d9f2b0de2e63527fa5aa2d12
Javorskinha <andrielijavorski@gmail.com>
2025-04-08 14:25:22 -0300

    turn css in bootstrap

--- src/components/UpdatedComponent.vue
+++ src/components/UpdatedComponent.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="updated">
+    <div class="bg-light text-center p-5">
         <div>
             <h2>Não perca nenhuma novidade! </h2>
             <p>Receba em primeira mão ofertas especiais, novos produtos e descontos especiais para inscritos!</p>
         </div>
-        <div class="subscribe">
+        <div class="col-md-7 m-auto">
             <InputComponent type="text" placeholder="Cadastre seu email"/>
-            <ButtonComponent text="Inscrever" class="blue button"/>
+            <ButtonComponent text="Inscrever" class="btn btn-outline-primary"/>
         </div>
-        <p class="terms">Ao assinar, você concorda com nossos Termos de Serviço e Política de Privacidade.</p>
+        <p><small>Ao assinar, você concorda com nossos Termos de Serviço e Política de Privacidade.</small></p>
     </div>
 </template>
 
@@ -16,28 +16,3 @@
 import InputComponent from './InputComponent.vue';
 import ButtonComponent from './ButtonComponent.vue';
 </script>
-
-<style scoped>
-    .updated{
-        background-color: var(--color-light-gray);
-        text-align: center;
-        padding: 3em 0;
-        margin: 2em 0;
-    }
-
-    .terms{
-        font-size: 0.6em;
-    }
-
-    @media screen and (min-width: 600px) {
-        .subscribe{
-            margin: 0 20%;
-        }
-    }
-
-    @media screen and (min-width: 1200px) {
-        .subscribe{
-            margin: 0 35%;
-        }
-    }
-</style>
