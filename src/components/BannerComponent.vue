d9350e6fe1b324cf1b0dd8d32c9131593aa7eb3b
javorskinha <andrielijavorski@gmail.com>
2025-04-19 16:32:23 -0300

    responsive banner for mobile

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="container">
-        <div class="row bg-white m-3 w-100 justify-content-center">
-            <div class="col-md-8 content-box bg-image-1 rounded-3 d-flex align-items-center">
-                <div class="d-flex flex-column w-50 p-5 text-white">
+        <div class="row bg-white m-1 m-md-3 w-100 justify-content-center">
+            <div class="col col-md-8 content-box bg-image-1 rounded-3 d-flex align-items-center">
+                <div class="d-flex flex-column w-50 p-md-5 text-white">
                     <h3 class="fw-bold display-6">Organicos Saudáveis e Frescos</h3>
                     <div class="border-start border-2 ps-1 mb-4">
                         <p class="lead text-shadow">Ofertas com <span class="bg-warning p-1 rounded-2">30%off</span></p>
                         <p><small>Entrega grátis a partir de R$150,00</small></p>
                     </div>
                     <div class="me-4">
-                        <ButtonComponent text="Comprar Agora"  class="btn bg-white rounded-5 text-primary p-2" icon="pi pi-chevron-right"/>
+                        <ButtonComponent text="Ver Mais" class="btn bg-white rounded-5 text-primary p-2" icon="pi pi-chevron-right"/>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 d-flex flex-column justify-content-between">
+            <div class="col-md-4 d-flex flex-column justify-content-between mt-3 mt-md-0 p-0 ps-md-3 pe-md-2">
                 <div class="bg-image-2 custom-height rounded-2 d-flex justify-content-between">
                     <div class="d-flex flex-column w-50 justify-content-center h-75 align-items-start p-3">
-                        <p class="mb-1"><small>Super Outono</small></p>
+                        <p class="mt-3 mb-1"><small>Super Outono</small></p>
                         <h3 class="fw-bold">75%OFF</h3>
                         <p class="text-secondary">Frutas e Vegetais</p>
                         <ButtonComponent text="Ver Mais" icon="pi pi-chevron-right" class="text-primary"/>
