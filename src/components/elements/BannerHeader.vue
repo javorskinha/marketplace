5def5b8200311ff122f6a36b7c78ecf640a527de
javorskinha <andrielijavorski@gmail.com>
2025-04-26 16:52:14 -0300

    created banner header

---
+++ src/components/elements/BannerHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <img src="../../../public/images/banner-header.jpg" alt="">
+</template>
+
+<style scoped>
+    img{
+        width: 100%;
+        height: 100px;
+        object-fit: cover;
+    }
+</style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,9 +1,11 @@
 <template>
+    <BannerHeader />
     <ProductsComponent />
     <UpdatedComponent />
 </template>
 
 <script setup>
 import ProductsComponent from '@/components/ProductsComponent.vue';
+import BannerHeader from '@/components/elements/BannerHeader.vue';
 import UpdatedComponent from '@/components/elements/UpdatedComponent.vue';
 </script>
