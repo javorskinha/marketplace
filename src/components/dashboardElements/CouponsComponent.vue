5a081afb50ea64a3ac8f34f58454b4569e3723ad
javorskinha <andrielijavorski@gmail.com>
2025-04-29 03:53:23 -0300

    created CouponsComponent.vue

--- src/components/dashboardElements/CouponsComponent.vue
+++ src/components/dashboardElements/CouponsComponent.vue
@@ -1,65 +1,132 @@
 <template>
-    <div class="container h-100">
-        <div class="w-100 d-flex justify-content-between">
+    <div class="container">
+        <div class="w-100 mb-3 d-flex justify-content-between">
             <h4>Cupons</h4>
+            <button class="btn btn-info" @click="openModal()">Novo Cupom <i class="pi pi-plus"></i></button>
         </div>
-        <div class="h-75 d-flex align-items-center justify-content-around">
+        <div class="d-flex align-items-center justify-content-around">
             <div>
-                <div v-if="allCoupons.length > 0">
-                    <div v-for="coupon in allCoupons" :key="coupon.id">
-                        <p class="mb-1"><strong>Código:</strong> {{ coupon.code }}</p>
-                        <p class="mb-1"><strong>Porcentagem:</strong> {{ coupon.discount_percentage }}</p>
-                        <p class="mb-3"><strong>Data de inicio:</strong> {{ coupon.start_date }}</p>
-                        <p class="mb-3"><strong>Data de fim:</strong> {{ coupon.end_date }}</p>
+                <div v-if="allCoupons.length > 0" class="row g-3">
+                    <div v-for="(coupon, index) in allCoupons" :key="index" class="col-12 col-md-6 col-lg-4 position-relative">
+                        <div class="card h-100 shadow-sm p-3 border-success">
+                            <h5 class="card-title mb-2"><strong>Código:</strong> {{ coupon.code }}</h5>
+                            <p class="mb-1"><strong>Porcentagem:</strong> {{ coupon.discount_percentage }}</p>
+                            <p class="mb-1"><strong>Data de inicio:</strong> {{ coupon.start_date }}</p>
+                            <p class="mb-1"><strong>Data de fim:</strong> {{ coupon.end_date }}</p>
+                        </div>
+                        <div class="position-absolute top-0 end-0 mt-2 m-3 d-flex flex-column gap-2">
+                            <button @click="openModal(coupon.id)" class="btn btn-outline-primary btn-sm h-25"><i class="pi pi-pen-to-square"></i></button>
+                            <button @click="delCoupon(coupon)" class="btn btn-outline-danger btn-sm h-25"><i class="pi pi-trash"></i></button>
+                        </div>
                     </div>
                 </div>
-                <div class="h-100 d-flex">
-                    <p class="fs-5 text-center fw-light">Não existem cupons cadastrados no momento  <i class="pi pi-gift fs-6 text-info"></i>
+                <div v-else class="h-100 d-flex">
+                    <p class="fs-5 text-center fw-light">Não existem cupons cadastrados no momento  <i class="pi pi-percentage fs-6 text-info"></i>
                     </p>
                 </div>
             </div>
-            <div class="border rounded-1 p-3 h-100">
-                <form @submit.prevent="saveNewData(newCoupon)">
-                    <h5>Criar Novo Cupom</h5>
-                    <div class="mb-2">
-                        <label for="form-label">Código</label>
-                        <input v-model="newCoupon.code" class="form-control"/>
-                    </div>
-                    <div class="mb-2 d-flex">
-                        <label for="form-label">Porcentagem</label>
-                        <input v-model="newCoupon.discount_percentage" class="form-control w-25 mx-3"/>
-                    </div>
-                    <div class="w-100 d-flex">
-                        <div class="mb-2 d-flex align-items-baseline">
-                            <label for="form-label">Início</label>
-                            <input type="date" v-model="newCoupon.start_date" class="form-control mx-3 h-75"/>
-                        </div>
-                        <div class="mb-2 d-flex align-items-baseline">
-                            <label for="form-label">Término</label>
-                            <input type="date" v-model="newCoupon.end_date" class="form-control mx-3 h-75"/>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-info w-100 mt-2 h-25">
-                        Salvar
-                    </button>
-                </form>
-            </div>
+            <DefaultModal :isVisible="isModalOpen" :title="isEditing ? 'Editar Cupom' : 'Criar Novo Cupom'" @close="closeModal">
+                <div class="border rounded-1">
+                    <DefaultForm
+                    :title="isEditing ? 'Edite os Dados' : 'Preencha os Dados'"
+                    :fields="fields"
+                    :formData="formData"
+                    @submit="handleFormSubmit"
+                    />
+                </div>
+            </DefaultModal>
         </div>
     </div>
 </template>
 
 <script setup>
 import { useProductsStore } from '@/stores/ProductsStore';
-import { computed, onMounted, reactive } from 'vue';
+import { computed, onMounted, reactive, ref } from 'vue';
+import DefaultForm from '../elements/DefaultForm.vue';
+import DefaultModal from '../elements/DefaultModal.vue';
 
 const productsStore = useProductsStore();
-const allCoupons = computed (()=> productsStore.coupons || []);
-const newCoupon = reactive({
+const isModalOpen = ref(false);
+const isEditing = ref(false);
+const currentCouponId = ref(null);
+const allCoupons = computed(() => {
+    return (productsStore.coupons || [])
+});
+
+const formData = reactive({
     code: '',
     discount_percentage: null,
     start_date: '',
-    end_date: ''
+    end_date: '',
 });
+
+const openModal = (couponId = null) => {
+    if (couponId) {
+        isEditing.value = true;
+        const coupon = allCoupons.value.find(c => c.id === couponId);
+        if (coupon) {
+            formData.code = coupon.code;
+            formData.discount_percentage = coupon.discount_percentage;
+            formData.start_date = coupon.start_date;
+            formData.end_date = coupon.end_date;
+            currentCouponId.value = couponId;
+        }
+    } else {
+        isEditing.value = false;
+        resetForm();
+    }
+    isModalOpen.value = true;
+};
+
+const closeModal = () => {
+    isModalOpen.value = false;
+    resetForm();
+};
+
+const fields = [
+    { type: 'text', label: 'Código', model: 'code', placeholder: 'Digite o código' },
+    { type: 'number', label: 'Porcentagem', model: 'discount_percentage', placeholder: 'Digite a porcentagem' },
+    { type: 'date', label: 'Data de Início', model: 'start_date' },
+    { type: 'date', label: 'Data de Término', model: 'end_date' },
+];
+
+function resetForm() {
+    formData.code = '';
+    formData.discount_percentage = null;
+    formData.start_date = '';
+    formData.end_date = '';
+    currentCouponId.value = null;
+}
+
+const handleFormSubmit = async (formData) => {
+    try {
+        const payload = {
+            code: formData.code,
+            discount_percentage: Number(formData.discount_percentage),
+            start_date: new Date(formData.start_date).toISOString(),
+            end_date: new Date(formData.end_date).toISOString(),
+        };
+
+        if (isEditing.value) {
+            await productsStore.updateCoupons({
+                action: "update",
+                couponData: { id: currentCouponId.value},
+                newCouponData: payload
+            });
+        } else {
+            await productsStore.updateCoupons({
+                action: "create",
+                couponData: payload
+            });
+        }
+
+        await getAllCoupons();
+        closeModal();
+
+    } catch (error) {
+        console.error("Erro ao salvar cupom", error);
+    }
+};
 
 async function getAllCoupons() {
     await productsStore.updateCoupons({
@@ -67,30 +134,13 @@ async function getAllCoupons() {
     });
 };
 
-const formatDate = (date) => {
-    const d = new Date(date);
-    return d.toISOString();
-};
-
-async function saveNewData(newCoupon) {
-    try {
-        const payload = {
-            code: newCoupon.code,
-            discount_percentage: newCoupon.discount_percentage,
-            start_date: formatDate(newCoupon.start_date),
-            end_date: formatDate(newCoupon.end_date),
-        };
-
-        await productsStore.updateCoupons({
-            action: "create",
-            couponData: payload
+async function delCoupon(coupon) {
+    await productsStore.updateCoupons({
+        action: "delete",
+        couponData: { id: coupon.id },
     });
-    
-    getAllCoupons();
 
-    } catch (error) {
-        console.error("Erro ao salvar cupom", error);
-    }
+    getAllCoupons();
 }
 
 onMounted(()=>{
