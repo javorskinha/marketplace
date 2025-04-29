b2cc3d3e5f41ab4fda500152fd030fd5d99776df
javorskinha <andrielijavorski@gmail.com>
2025-04-29 19:51:14 -0300

    fix router

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup>
-import HeaderComponent from './components/HeaderComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
 import { RouterView } from 'vue-router';
 import FooterComponent from './components/FooterComponent.vue';
 </script>
