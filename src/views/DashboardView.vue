978b5f4a7211b3b6c316c97ea2ec42d378dd7793
javorskinha <andrielijavorski@gmail.com>
2025-04-29 21:23:44 -0300

    update dashboardView

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,14 @@
 <template>
     <DashboardComponent/>
+    <EnphasisComponent 
+    title="Produtos em destaque"
+    button="VEJA TODOS OS PRODUTOS"
+    />
+    <UpdatedComponent />
 </template>
 
 <script setup>
 import DashboardComponent from '@/components/DashboardComponent.vue';
+import EnphasisComponent from '@/components/elements/EnphasisComponent.vue';
+import UpdatedComponent from '@/components/elements/UpdatedComponent.vue';
 </script>
